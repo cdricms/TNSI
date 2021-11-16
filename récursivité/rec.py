7be654bcd1eb189a4b27b158c2275887dcf90d4a
Smoqu <36056008+Smoqu@users.noreply.github.com>
2021-11-16 12:10:31 +0100

    EP

--- récursivité/rec.py
+++ récursivité/rec.py
@@ -77,3 +77,99 @@ def facto_iter(n):
 
 
 print(facto_iter(8))
+
+
+def f91(n: int):
+    if n > 100:
+        return n - 10
+    return f91(f91(n+11))
+    # Conjecturation de f91(n) avec n <= 101, le résultat sera toujours 91
+
+
+def f91_detail(n, k=1):
+    result = n
+    for i in range(k):
+        result = f91(result)
+
+    return result
+
+
+print(f91_detail(102, 3))
+print(f91(f91(f91(102))))
+
+
+def F(n: int):
+    if n == 0:
+        return 1
+    elif n > 0:
+        return n - M(F(n-1))
+
+
+def M(n: int):
+    if n == 0:
+        return 0
+    elif n > 0:
+        return n - F(M(n-1))
+
+
+print(F(9))
+print(M(9))
+
+
+def syracuse(u: int):
+    print(u)
+    if u != 1:
+        if u % 2 == 0:
+            syracuse(u/2)
+        else:
+            syracuse(3 * u + 1)
+
+
+# syracuse(5)
+# syracuse(548)
+
+
+def duree_vol(u: int):
+    if u == 1:
+        return 0
+    if u % 2 == 0:
+        return duree_vol(u / 2)+1
+    else:
+        return duree_vol(3 * u + 1)+1
+
+
+def dv(u: int):
+    c = u
+    highest = 0
+    counter = 0
+    while c != 1:
+        counter += 1
+        if c % 2 == 0:
+            c /= 2
+        else:
+            c = c * 3 + 1
+
+        if highest < c:
+            highest = c
+
+    print(highest)
+
+    return counter
+
+
+print(duree_vol(5))
+print(dv(674))
+
+
+def altitude(u: int):
+    if u == 1:
+        return 0
+
+    if u % 2 == 0:
+        new_value = altitude(u / 2)
+    else:
+        new_value = altitude(u * 3 + 1)
+    return new_value if new_value > u else u
+
+
+print(altitude(674))
